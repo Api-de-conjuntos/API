8553176f917a29c551d9ff5a22a08673172214af
Cleiton Luiz <39466647+cleiton016@users.noreply.github.com>
2020-02-14 20:49:58 -0300

    Commit - Novas funções
    Foi incrementado as seguintes funções:
    -pertence(valor: String)
    -eh_subconjunto(valor: array)
    -contem_propriamente(valor: array)

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-class conjunto():
+class Conjunto():
     def __init__(self,nome):
         self.nome = nome
         self.elementos = list()
@@ -9,4 +9,25 @@ class conjunto():
         return len(self.elementos)
     def imprimir(self):
         print('Nome do conjunto:{}\nElementos:{}'.format(self.nome,self.elementos))
+    def pertence(self, valor):
+        if(valor in self.elementos):
+            return True
+        else:
+            return False
+    def eh_subconjunto(self, valor):
+        for i in valor:
+            if i in self.elementos:
+                return True
+        return False
+    def contem_propriamente(self, valor):
+        retorno = []
+        if len(valor) == len(self.elementos):
+            return False
+        else:
+            for i in valor:
+                if i in self.elementos:
+                    pass
+                else:
+                    return False
+            return True
 
--- teste.py
+++ teste.py
@@ -1,11 +1,14 @@
 import api
-a = api.conjunto('vogais')#É passado como parametro o nome do conjunto
+a = api.Conjunto('vogais')#É passado como parametro o nome do conjunto
 #Função responsavel por inserir elementos no conjunto
 a.push('a')
 a.push('e')
 a.push('i')
-#aqui é inserido um elemento que ja existe no conjunto. OBS: elemento não sera repetido
-a.push('a')
+a.push('a')#aqui é inserido um elemento que ja existe no conjunto. OBS: elemento não sera repetido
 a.push('o')
 a.push('u')
+print('Tamanho',a.tamanho())
 a.imprimir()
+print('Pertence:',a.pertence('c'))
+print('subconjunto',a.eh_subconjunto(['b','d','c']))
+print('Contem propriamente:',a.contem_propriamente(['a','e','i','o']))
